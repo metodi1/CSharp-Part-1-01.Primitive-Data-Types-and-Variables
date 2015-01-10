5797c214ca94836191e798918ed4a4e064dc7ead
metodi1 <metodi.ganev@gmail.com>
2015-01-10 18:42:28 +0200

    creation

---
+++ Problem 1. Declare Variables/Problem 1. Declare Variables/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Problem_1.Declare_Variables
+{
+    class Program
+    {
+        static void Main()
+        {
+//Declare five variables choosing for each of them the most appropriate of the types byte, sbyte, short, 
+//ushort, int, uint, long, ulong to represent the following values: 52130, -115, 4 825 932, 97, -10000.
+//Choose a large enough type for each number to ensure it will fit in it. Try to compile the code.
+//Submit the source code of your Visual Studio project as part of your homework submission.
+
+            ushort number1 = 52130;
+            sbyte number2 = -115;
+            int number3 = 4825932;
+            sbyte number4=97;
+            short number5=-10000;
+
+            Console.WriteLine("{0} is ushort, {1} is sbyte, {2} is int, {3} is sbyte, {4} is short.", number1, number2, number3, number4, number5);
+            
+
+        }
+    }
+}
