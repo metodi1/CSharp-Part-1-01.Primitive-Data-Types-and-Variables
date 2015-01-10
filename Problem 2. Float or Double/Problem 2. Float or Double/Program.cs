a735dc1f5d5d09bc4dd6e1d770cce705e817a85f
metodi1 <metodi.ganev@gmail.com>
2015-01-10 19:13:45 +0200

    change

---
+++ Problem 2. Float or Double/Problem 2. Float or Double/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Problem_2.Float_or_Double
+{
+    class Program
+    {
+        static void Main()
+        {
+            //Which of the following values can be assigned to a variable of type float and which to a variable
+            //of type double: 34.567839023, 12.345, 8923.1234857, 3456.091?Write a program to assign the numbers in 
+            //variables and print them to ensure no precision is lost.
+
+            double number1 = 34.567839023;
+            float number2 = 12.345f;
+            double number3 = 8923.1234857f;
+            float number4 = 3456.091f;
+
+            Console.WriteLine("{0} is double, {1} is foat, {2} is foat, {3} is float", number1, number2, number3, number4);
+
+        }
+    }
+}
