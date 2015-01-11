5f6017a7ce65ce13923b893beb6f33ff7f2ff652
metodi1 <metodi.ganev@gmail.com>
2015-01-11 11:35:58 +0200

    creation

---
+++ Problem 5. Boolean Variable/Problem 5. Boolean Variable/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+
+class Program
+{
+    static void Main()
+//Declare a Boolean variable called isFemale and assign an appropriate value corresponding to your gender.
+//Print it on the console.
+    {
+            
+        bool isFemale=false;
+        Console.WriteLine("is it true that I'm a woman:{0}", isFemale);
+    }
+}
---
+++ Problem 6. Strings and Objects/Problem 6. Strings and Objects/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+
+
+class Program
+{
+    static void Main()
+    {
+//Declare two string variables and assign them with Hello and World.
+//Declare an object variable and assign it with the concatenation of the first two variables (mind adding an interval between).
+//Declare a third string variable and initialize it with the value of the object variable (you should perform type casting).
+
+        string var1="Hello";
+        string var2 = "World";
+        object concatenation=var1+' '+var2 ;
+        string var3 = (string)concatenation;
+        Console.WriteLine(var3);
+    }
+}
+
+
