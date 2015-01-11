21b9bb2c17a0f7a27120d11e55e68963d3fd76b3
metodi1 <metodi.ganev@gmail.com>
2015-01-11 15:45:44 +0200

    creation

---
+++ Problem 3. Variable in Hexadecimal Format/Problem 3. Variable in Hexadecimal Format/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+
+
+class Program
+{
+    static void Main()
+    //Declare an integer variable and assign it with the value 254 in hexadecimal format (0x##).
+    //Use Windows Calculator to find its hexadecimal representation.
+    //Print the variable and ensure that the result is 254.
+    {
+        
+       
+        int value = Convert.ToInt32("0xFE", 16);
+        Console.WriteLine(value);
+    }
+
+}
+
