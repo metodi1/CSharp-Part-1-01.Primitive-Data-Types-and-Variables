b6fb37f761e2dbddf703224c01778d643bb883b5
metodi1 <metodi.ganev@gmail.com>
2015-01-11 10:55:44 +0200

    creation

---
+++ Problem 4. Unicode Character/Problem 4. Unicode Character/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+//Declare a character variable and assign it with the symbol that has Unicode code 42 (decimal) using the \u00XX syntax, and then print it.
+//Hint: first, use the Windows Calculator to find the hexadecimal representation of 42. The output should be *.
+
+class Program
+{
+    static void Main()
+    {
+        
+        char star = '\u002A';
+        Console.WriteLine(star);
+      
+        }
+    }
+
