a134a9757026b0d42587a213e388620ebc16f7fa
metodi1 <metodi.ganev@gmail.com>
2015-01-11 17:04:31 +0200

    v1

--- Problem 8. Isosceles Triangle/Problem 8. Isosceles Triangle/Program.cs
+++ Problem 8. Isosceles Triangle/Problem 8. Isosceles Triangle/Program.cs
@@ -1,7 +1,16 @@
 ﻿using System;
 using System.Text;
-
-
+//ite a program that prints an isosceles triangle of 9 copyright symbols ©, something like this:
+//   ©
+//
+//  © ©
+//
+// ©   ©
+//
+//© © © ©
+//Note: The © symbol may be displayed incorrectly at the console so you may need
+//to change the console character encoding to UTF-8 and assign a Unicode-friendly font in the console.
+//Note: Under old versions of Windows the © symbol may still be displayed incorrectly, regardless of how much effort you put to fix it.
 
 class Program
 {
