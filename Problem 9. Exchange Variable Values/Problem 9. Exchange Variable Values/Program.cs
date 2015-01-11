755a137a1bb6d4cd79bf6516609ea26e9d94545b
metodi1 <metodi.ganev@gmail.com>
2015-01-11 17:08:15 +0200

    creation

---
+++ Problem 9. Exchange Variable Values/Problem 9. Exchange Variable Values/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+//Declare two integer variables a and b and assign them with 5 and 10 and after 
+//that exchange their values by using some programming logic.
+//Print the variable values before and after the exchange.
+
+class Problem_9_Exchange_Variable_Values
+{
+    static void Main()
+    {
+    }
+}
