18d9fb4742b83f303d42df8ef073cb5d87bd7fba
metodi1 <metodi.ganev@gmail.com>
2015-01-11 16:05:23 +0200

    creation

--- Problem 7. Quotes in Strings/Problem 7. Quotes in Strings/Program.cs
+++ Problem 7. Quotes in Strings/Problem 7. Quotes in Strings/Program.cs
@@ -7,6 +7,13 @@
 //Declare two string variables and assign them with following value: The "use" of quotations causes difficulties.
 //Do the above in two different ways - with and without using quoted strings.
 //Print the variables to ensure that their value was correctly defined.
+
+            string withQuoted = "The \"use\" of quotations causes difficulties.";
+            string withoutQuoted = "The use of quotations causes difficulties.";
+
+            Console.WriteLine(withQuoted);
+            Console.WriteLine(withoutQuoted);
+
         }
     
     }
