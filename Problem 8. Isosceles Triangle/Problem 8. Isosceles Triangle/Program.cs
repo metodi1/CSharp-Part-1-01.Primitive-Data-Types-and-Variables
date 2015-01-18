e438d872add05b6ef2c167eebdd2124933e6ed94
metodi1 <metodi.ganev@gmail.com>
2015-01-18 11:50:01 +0200

    1

--- Problem 8. Isosceles Triangle/Problem 8. Isosceles Triangle/Program.cs
+++ Problem 8. Isosceles Triangle/Problem 8. Isosceles Triangle/Program.cs
@@ -19,13 +19,12 @@ class Program
 
         Console.OutputEncoding = Encoding.UTF8;
         char symbol = '©';
-        Console.WriteLine("  {0}\n {1}{2}{3}\n{4}{5}{6}{7}{8}\n",
+        Console.WriteLine("   {0}    \n\n  {1} {2}  \n\n {3}   {4} \n\n{5} {6} {7} {8}",
             symbol, symbol, symbol, symbol, symbol, symbol, symbol, symbol, symbol);
     }
 
 
 }
-
 
 
 
